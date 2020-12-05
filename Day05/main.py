f6242d2e8382f23a0fd3f09292bde734ae801354
chriswasser <christian_wassermann@web.de>
2020-12-05 10:59:10 +0100

    Add revised solution with tests for Day05

--- Day05/main.py
+++ Day05/main.py
@@ -3,15 +3,27 @@
 import fileinput
 
 
+def seat_from_ticket(ticket):
+    return int(ticket.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), base=2)
+
+
+def find_missing_seat(seats):
+    seats = sorted(seats)
+    for seat1, seat2 in zip(seats[:-1], seats[1:]):
+        if seat1 + 1 != seat2:
+            return seat1 + 1
+
+
 def test_task1():
+    assert seat_from_ticket('FBFBBFFRLR') == 357
+    assert seat_from_ticket('BFFFBBFRRR') == 567
+    assert seat_from_ticket('FFFBBBFRRR') == 119
+    assert seat_from_ticket('BBFFBBFRLL') == 820
     print('tests for task 1: ok')
 
 
 def solve_task1():
-    seats = []
-    for line in fileinput.input():
-        line = line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1')
-        seats.append(int(line, base=2))
+    seats = [seat_from_ticket(line.rstrip()) for line in fileinput.input()]
     solution = max(seats)
     print(f'answer to task 1: {solution}')
 
@@ -21,18 +33,8 @@ def test_task2():
 
 
 def solve_task2():
-    seats = []
-    for line in fileinput.input():
-        line = line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1')
-        seats.append(int(line, base=2))
-    seats.sort()
-    last = seats[0]
-    for seat in seats[1:]:
-        if seat == last + 1:
-            last = seat
-        else:
-            solution = last + 1
-            break
+    seats = [seat_from_ticket(line.rstrip()) for line in fileinput.input()]
+    solution = find_missing_seat(seats)
     print(f'answer to task 2: {solution}')
 
 
